dbd2553070a8c70873007f9ca377bb088251e408
EmilyGG24308 <gon24308@uvg.edu.gt>
2026-01-24 17:55:54 -0600

    Add files via upload

---
+++ Busqueda_indexada.py
@@ -0,0 +1,44 @@
+import os
+import time
+
+def busqueda_indexada(carne_buscado):
+    archivos_recorridos = 0
+    lineas_recorridas = 0
+    inicio = time.perf_counter()
+
+    
+    with open("indice_estudiantes.txt", "r", encoding="utf-8") as indice:
+        for linea in indice:
+            lineas_recorridas += 1
+            carne, archivo, offset = linea.strip().split("|")
+
+            if carne == carne_buscado:
+                archivos_recorridos = 1 
+                ruta = os.path.join("datos", archivo)
+
+                
+                with open(ruta, "r", encoding="utf-8") as f:
+                    f.seek(int(offset))
+                    registro = ""
+
+                    while True:
+                        char = f.read(1)
+                        if char == "|" or char == "":
+                            break
+                        registro += char
+
+                fin = time.perf_counter()
+                tiempo_ejecucion = fin - inicio
+
+                
+                partes = registro.split("-")
+                nombre = partes[1] if len(partes) > 1 else "Desconocido"
+
+                return (
+                    f"Estudiante Encontrado: {nombre} con carné {carne}. "
+                    f"Archivos Recorridos: {archivos_recorridos}, "
+                    f"Líneas Recorridas (índice): {lineas_recorridas}, "
+                    f"Tiempo de Ejecución: {tiempo_ejecucion:.6f} segundos"
+                )
+
+    return "Estudiante no encontrado"
